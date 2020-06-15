18de783c31355f2e337c4743d228b9811baf9434
Gabriel Madeira <gabr.madeira@gmail.com>
2020-06-15 17:34:14 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'thegoldtree-rs';
+  RSrequest;
+  rsTitle = 'Um método para deduplicação de metadados bibliográficos baseado no empilhamento de classificadores';  ;
+  rsAbstract;
+  
+
+  OnSubmit() {
+    console.log("test");
+    this.http.post('http://127.0.0.1:5000/rs', { titleabstract : this.rsTitle + this.rsAbstract }).subscribe(data => {
+      this.RSrequest = data;
+    })
+    console.log(this.RSrequest);
+  }
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {     
+
+  }
 }
